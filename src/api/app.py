9dae89d36452c265f10c6acee43ad74003361955
PianoNic <79938743+Pianonic@users.noreply.github.com>
2025-09-09 19:41:50 +0200

    refactor: ensure compatibility with OpenAPI schema
    resolve #32

--- src/api/app.py
+++ src/api/app.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-from slowapi.errors import RateLimitExceeded
+from fastapi import FastAPI
 from src.application.services.db_service import setup_db
 from src.application.services.env_service import load_env
 from src.api.router_registry import router_registry
 from src.infrastructure.logging_config import setup_colored_logging
+from fastapi.openapi.utils import get_openapi
 
 setup_colored_logging()
 
@@ -12,11 +11,30 @@ load_env()
 setup_db()
 
 app = FastAPI(
-    title="Schulnetz API Wrapper",
-    description="A FastAPI application to wrap Schulnetz API endpoints.",
+    title="Schulware API Wrapper",
+    description="A FastAPI application to wrap Schulware API endpoints.",
     version="1.0.0",
     redoc_url=None,
     docs_url="/"
 )
 
+# TEMPORARY anyOf/oneOf workaround for Dart code generator compatibility
+def custom_openapi():
+    data = get_openapi(
+        title=app.title,
+        version=app.version,
+        description=app.description,
+        routes=app.routes,
+    )
+    if (("components" in data) and ("schemas" in data["components"])
+            and ("ValidationError" in data["components"]["schemas"])
+            and ("properties" in data["components"]["schemas"]["ValidationError"])
+                and ("loc" in data["components"]["schemas"]["ValidationError"]["properties"])
+                and ("items" in data["components"]["schemas"]["ValidationError"]["properties"]["loc"])):
+        data["components"]["schemas"]["ValidationError"]["properties"]["loc"]["items"] = {"type": "string"}
+    return data
+
+
 router_registry.auto_register(app)
+
+app.openapi = custom_openapi
