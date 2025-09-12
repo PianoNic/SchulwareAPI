ea2e9754858ff2be26e913803cbf7e32fc3cdf6a
Z Ξ N I X <79938743+PianoNic@users.noreply.github.com>
2025-09-12 21:32:24 +0200

    Enhancement/48 added lateness dto (#49)
    * Added Lateness endpoint
    
    resolve #48
    
    * Fix LatenessDto field order by moving dateExcused above date

--- src/application/dtos/mobile/lateness_dto.py
+++ src/application/dtos/mobile/lateness_dto.py
@@ -1,4 +1,17 @@
 from pydantic import BaseModel
+from typing import Optional
+from datetime import date, time
 
 class LatenessDto(BaseModel):
-    pass
+    id: str
+    dateExcused: Optional[date]
+    date: date
+    startTime: time
+    endTime: time
+    duration: str
+    reason: Optional[str]
+    excused: bool
+    extendedDeadline: int
+    courseId: str
+    courseToken: str
+    comment: Optional[str]
