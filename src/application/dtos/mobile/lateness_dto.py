db0020320ce4911b14a23d12c307ecb605472740
PianoNic <79938743+Pianonic@users.noreply.github.com>
2025-09-12 21:32:24 +0200

    Fix LatenessDto field order by moving dateExcused above date

--- src/application/dtos/mobile/lateness_dto.py
+++ src/application/dtos/mobile/lateness_dto.py
@@ -4,13 +4,13 @@ from datetime import date, time
 
 class LatenessDto(BaseModel):
     id: str
+    dateExcused: Optional[date]
     date: date
     startTime: time
     endTime: time
     duration: str
     reason: Optional[str]
     excused: bool
-    dateExcused: Optional[date]
     extendedDeadline: int
     courseId: str
     courseToken: str
