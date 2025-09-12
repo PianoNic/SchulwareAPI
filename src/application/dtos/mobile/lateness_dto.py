7a83d61eeab5d549f6e0b264c397fc1a49b565b1
PianoNic <79938743+Pianonic@users.noreply.github.com>
2025-09-12 21:31:56 +0200

    Added Lateness endpoint
    resolve #48

--- src/application/dtos/mobile/lateness_dto.py
+++ src/application/dtos/mobile/lateness_dto.py
@@ -1,4 +1,17 @@
 from pydantic import BaseModel
+from typing import Optional
+from datetime import date, time
 
 class LatenessDto(BaseModel):
-    pass
+    id: str
+    date: date
+    startTime: time
+    endTime: time
+    duration: str
+    reason: Optional[str]
+    excused: bool
+    dateExcused: Optional[date]
+    extendedDeadline: int
+    courseId: str
+    courseToken: str
+    comment: Optional[str]
