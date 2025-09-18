6d52c76bb29e33dc2cf968abb5d5f374287bcec9
PianoNic <79938743+Pianonic@users.noreply.github.com>
2025-09-19 00:36:15 +0200

    Refactor mobile authentication endpoint to use Form parameters for email and password

--- src/api/controllers/auth_controller.py
+++ src/api/controllers/auth_controller.py
@@ -1,4 +1,4 @@
-from fastapi import Request, HTTPException, Response
+from fastapi import Form, Request, HTTPException, Response
 from src.application.commands.authenticate_mobile_command import authenticate_mobile_command_async
 from src.application.commands.authenticate_web_command import authenticate_web_command_async
 from src.application.commands.authenticate_unified_command import authenticate_unified_command_async
@@ -27,8 +27,8 @@ async def authenticate_unified_api(request: Request, auth_request: AuthenticateR
 
 @router.post("mobile", response_model=AuthenticateMobileResponseDto)
 @shared_limiter.limit("1/30seconds")
-async def authenticate_mobile_api(request: Request, auth_request: AuthenticateRequestDto):
-    return await authenticate_mobile_command_async(auth_request.email, auth_request.password)
+async def authenticate_mobile_api(request: Request, email: str = Form(...), password: str = Form(...)):
+    return await authenticate_mobile_command_async(email, password)
 
 @router.post("web")
 @shared_limiter.limit("1/30seconds")
