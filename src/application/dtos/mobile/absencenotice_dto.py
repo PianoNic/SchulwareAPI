619f8b4c5775f1837c08930facd03d7395c43b5c
PianoNic <79938743+Pianonic@users.noreply.github.com>
2025-09-18 17:36:36 +0200

    Added AbsencenoticeDTO
    resolve #51

--- src/application/dtos/mobile/absencenotice_dto.py
+++ src/application/dtos/mobile/absencenotice_dto.py
@@ -1,4 +1,25 @@
+from typing import Optional
 from pydantic import BaseModel
 
 class AbsenceNoticeDto(BaseModel):
-    pass
+    id: str
+    studentId: str
+    studentReason: Optional[str] = None
+    studentReasonTimestamp: Optional[str] = None
+    studentIs18: bool
+    date: str
+    hourFrom: str
+    hourTo: str
+    time: str
+    status: str
+    statusLong: str
+    comment: Optional[str] = None
+    isExamLesson: bool
+    profile: str
+    course: str
+    courseId: str
+    absenceId: str
+    absenceSemester: int
+    trainerAcknowledgement: Optional[str] = None
+    trainerComment: Optional[str] = None
+    trainerCommentTimestamp: Optional[str] = None
