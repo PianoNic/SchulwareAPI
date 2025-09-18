6df730c0dceea428e2e31b8ed8f3a1df592d679f
Z Ξ N I X <79938743+PianoNic@users.noreply.github.com>
2025-09-18 17:27:55 +0200

    Hided Uvicorn from the response header (#50)
    resolve #29

--- asgi.py
+++ asgi.py
@@ -5,4 +5,4 @@ from src.infrastructure.logging_config import setup_colored_logging
 if __name__ == "__main__":
     setup_colored_logging()
     
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_config=None, access_log=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_config=None, access_log=True, server_header=False)
